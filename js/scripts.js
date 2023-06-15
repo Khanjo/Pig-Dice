1d49e6e272195a1f92aaadd17bf30eb81aebdb95
Joshua Khan <Khanjo@gmail.com>
2023-06-15 11:57:29 -0700

    add form submission to update player names and start game

--- js/scripts.js
+++ js/scripts.js
@@ -17,25 +17,25 @@ function Player(turn) {
     this.turn = turn;
     this.playerName;
 }
-//check for a roll of 1 
+
 Player.prototype.rollone = function () {
     if (this.roll === 1) {
         this.tempscore = 0;
-        alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over")
+        alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
     } else {
         this.tempscore += this.roll;
     }
 }
-// hold 
+
 Player.prototype.hold = function () {
     this.totalscore += this.tempscore;
     this.tempscore = 0;
-    alert("your turn is over")
+    alert(this.playerName + ", your turn is over, pass the mouse!");
 }
 
 Player.prototype.winnerCheck = function () {
     if (this.totalscore >= 100) {
-        alert(this.playerName + " You are the winner!")
+        alert(this.playerName + " You are the winner!");
     }
 }
 
@@ -45,7 +45,49 @@ Player.prototype.newGame = function () {
     this.totalscore = 0;
     this.playerName = "";
 }
-/*
+
+//User Interface Logic
+
+window.addEventListener("load", function () {
+    let form = document.getElementById("playerNames");
+    let die = new Die();
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let player1 = document.getElementById("player1Name").value;
+        let player2 = document.getElementById("player2Name").value;
+        document.getElementById("p1").innerHTML = player1;
+        document.getElementById("p2").innerHTML = player2;
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 function Dice() {
     this.one = 1,
     this.two = 2,
@@ -53,12 +95,29 @@ function Dice() {
     this.four = 4,
     this.five = 5,
     this.six = 6
-}
+} 
 
 Dice.prototype.rollDice = function () {
     let keys = Object.keys(this);
     return this[keys[keys.length * Math.random() << 0]];
 };
-*/
 
-//User Interface Logic
+const init = function () {
+    score = [0, 0]
+    currentScore = 0
+    activePlayer = 0
+    playing = true
+
+    score0El.textContent = 0
+    score1El.textContent = 0
+    current0El.textContent = 0
+    current1El.textConent = 0
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner')
+    player1El.classList.remove('player--winner')
+    player0El.classList.add('player--active')
+    player1El.classList.remove('player--active')
+};
+init();
+*/
